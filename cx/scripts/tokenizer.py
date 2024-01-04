92924f0218074aafe96e21a4a7b73a3ca1d17a18
Jewels <107947731+Jewels333@users.noreply.github.com>
2024-01-03 21:30:16 -0500

    Update tokenizer.py
    Better tokens

--- cx/scripts/tokenizer.py
+++ cx/scripts/tokenizer.py
@@ -1,7 +1,20 @@
 from nltk.tokenize import word_tokenize
+import json
+
+f = open("cx/data.json")
+data = json.load(f)
 
 class Tokenizer:
     def lex(self, source: str):
         tokens = word_tokenize(source)
+        identifed = []
         
+        for token in tokens:
+            for type in data["types"]:
+                if token in type["tokens"]: 
+                    identified.append((token, type["name"]))
+                    continue
+                
+                
+                
         
